1378a75305fd7f3b470f48dacb74aa6c739b181c
kesakiyo <kesakiyo@naver.com>
2019-01-24 01:26:19 +0900

    BOJ 1032 문제 업데이트

---
+++ Baekjoon Online Judge/1032/boj-1032.cpp
@@ -0,0 +1,32 @@
+/*
+	Copyright(c) 2019. Minho Cheon. All rights reserved.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+string str[51];
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	cin >> n;
+	for (int i = 0 ; i < n ; ++i) {
+		cin >> str[i];
+	}
+
+	int len = str[0].length();
+	for (int i = 0 ; i < len ; ++i) {
+		const char ch = str[0][i];
+		bool flag = true;
+		for (int j = 1 ; j < n ; ++j) {
+			if (str[j][i] != ch) {
+				flag = false;
+				break;
+			}
+		}
+		cout << (flag ? ch : '?');
+	}
+}
