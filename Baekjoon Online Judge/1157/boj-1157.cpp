31ca7483f1536d7c63b01b3833d58d80f85fc26f
kesakiyo <kesakiyo@naver.com>
2019-01-24 01:45:59 +0900

    BOJ 1157 업데이트

---
+++ Baekjoon Online Judge/1157/boj-1157.cpp
@@ -0,0 +1,39 @@
+/*
+    Copyright(c) 2019. Minho Cheon. All rights reserved.
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    string str;
+    cin >> str;
+
+    int cnt[26];
+    fill(cnt, cnt + 26, 0);
+
+    for (int i = 0 ; i < str.length() ; ++i) {
+        char ch = str[i];
+        if ('a' <= ch && ch <= 'z') {
+            ch -= ('a' - 'A');
+        }
+        ++cnt[ch - 'A'];
+    }
+
+    int maxIdx = 0;
+    bool isMultiple = false;
+    for (int i = 1 ; i < 26 ; ++i) {
+        if (cnt[i] > cnt[maxIdx]) {
+            maxIdx = i;
+            isMultiple = false;
+        } else if (cnt[i] == cnt[maxIdx]) {
+            isMultiple = true;
+        }
+    }
+
+    cout << (isMultiple ? '?' : (char)(maxIdx + 'A')) << endl;
+}
