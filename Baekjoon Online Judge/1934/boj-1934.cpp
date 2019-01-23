e9a901fe85774f98232c646faea427bb2b51daa6
kesakiyo <kesakiyo@naver.com>
2019-01-24 01:50:43 +0900

    BOJ 1934 업데이트

---
+++ Baekjoon Online Judge/1934/boj-1934.cpp
@@ -0,0 +1,28 @@
+/*
+    Copyright(c) 2019. Minho Cheon. All rights reserved.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int getGcd(int a, int b) {
+    if (!b) {
+        return a;
+    }
+    return getGcd(b, a % b);
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        const int gcd = getGcd(a, b);
+        cout << (a * b) / gcd << endl;
+    }
+}
