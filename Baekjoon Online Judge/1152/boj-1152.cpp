c44b516c11416eb576097c7a1ac62a18feb67e11
kesakiyo <kesakiyo@naver.com>
2019-01-24 01:31:40 +0900

    BOJ 1152 문제 업데이트

---
+++ Baekjoon Online Judge/1152/boj-1152.cpp
@@ -0,0 +1,18 @@
+/*
+	Copyright(c) 2019. Minho Cheon. All rights reserved.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int cnt;
+string str;
+
+int main() {
+	while (cin >> str) {
+		++cnt;
+	}
+	cout << cnt << endl;
+}
