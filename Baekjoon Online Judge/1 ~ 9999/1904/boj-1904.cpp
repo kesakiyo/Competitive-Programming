c5bb3153aa1b0dd5bdfae53c46b02ea17663ffe0
kesakiyo <kesakiyo@naver.com>
2019-02-11 02:31:39 +0900

    BOJ 1904 업데이트

---
+++ Baekjoon Online Judge/1 ~ 9999/1904/boj-1904.cpp
@@ -0,0 +1,34 @@
+/*
+    Copyright(c) 2019. Minho Cheon. All rights reserved.
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int MOD = 15746;
+int n, cache[1000010];
+
+int func(int idx) {
+	if (idx == 0) {
+		return 1;
+	}
+
+	int& ret = cache[idx];
+	if (ret) {
+		return ret;
+	}
+
+	ret = func(idx - 1) % MOD;
+	if (idx >= 2) {
+		ret = (ret + func(idx - 2)) % MOD;
+	}
+	
+	return ret;
+}
+
+int main() {
+	cin >> n;
+	cout << func(n) << endl;
+}
