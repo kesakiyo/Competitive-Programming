3c6737f587a37f9e3a8a2d562da0a55fc1135d5d
kesakiyo <kesakiyo@naver.com>
2019-02-11 01:35:23 +0900

    BOJ 11055 업데이트

---
+++ Baekjoon Online Judge/10000 ~/11055/boj-11055.cpp
@@ -0,0 +1,40 @@
+/*
+    Copyright(c) 2019. Minho Cheon. All rights reserved.
+*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, A[1010], cache[1010];
+
+int func(int here) {
+	int& ret = cache[here];
+	if (ret) {
+		return ret;
+	}
+
+	ret = A[here];
+	for (int there = here + 1 ; there < n ; ++there) {
+		if (A[here] < A[there]) {
+			ret = max(ret, func(there) + A[here]);
+		}
+	}
+
+	return ret;
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0 ; i < n ; ++i) {
+		cin >> A[i];
+	}
+
+	int ans = 0;
+	for (int i = 0 ; i < n ; ++i) {
+		ans = max(ans, func(i));
+	}
+
+	cout << ans << endl;
+}
